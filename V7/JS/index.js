ddf758b37e749c185bf0a4c11faba7249a8d065e
DevRed91 <51876334+DevRed91@users.noreply.github.com>
2020-06-10 19:12:37 +0530

    Update index.js

--- V7/JS/index.js
+++ V7/JS/index.js
@@ -2,11 +2,11 @@ import * as THREE from '../three/build/three.module.js'
 import {OrbitControls} from '../three/examples/jsm/controls/OrbitControls.js';
 import { RectAreaLightUniformsLib } from '../three/examples/jsm/lights/RectAreaLightUniformsLib.js';
 import {CreateWall} from './CreateWall.js';
+import {CreateFloor} from './CreateFloor.js';
+import {CreateCornice} from './CreateCornice.js';
 
     let scene, camera;
     let renderer, raycaster;
-
-    let group;
 
     function init() {
 
@@ -45,118 +45,6 @@ import {CreateWall} from './CreateWall.js';
         //grid
         // let gridHelper = new THREE.GridHelper(10000, 20);
         // scene.add(gridHelper);
-        
-        //group
-        group = new THREE.Group();
-        scene.add(group);
-
-        let wallMaterial = new THREE.MeshPhongMaterial( {
-                        color: 0xffffff,
-                        specular: 0x009900,
-                        shininess: 30,
-                        side: THREE.FrontSide
-                    });
-        
-        // walls
-        let wallGeo1 = new THREE.PlaneBufferGeometry(214.5, 300, 1);
-        let wall1 = new CreateWall(wallGeo1, wallMaterial, [224.4,150,-299], 0);
-        scene.add(wall1);
-
-        let wallGeo2 = new THREE.PlaneBufferGeometry(304, 300, 1);
-        let wall2 = new CreateWall(wallGeo2, wallMaterial, [117.2,150,-150], Math.PI/2);
-        scene.add(wall2);
-
-        let wallGeo3 = new THREE.PlaneBufferGeometry(234.4, 300, 1);
-        let wall3 = new CreateWall(wallGeo3, wallMaterial, [0,150,2.5], 0);
-        scene.add(wall3);
-
-        let wallGeo4 = new THREE.PlaneBufferGeometry(304, 300, 1);
-        let wall4 = new CreateWall(wallGeo4, wallMaterial, [-117.2,150,-150], -Math.PI/2);
-        scene.add(wall4);
-
-        let wallGeo5 = new THREE.PlaneBufferGeometry(214.5, 300, 1);
-        let wall5 = new CreateWall(wallGeo5, wallMaterial, [-224.4,150,-299], 0);
-        scene.add(wall5);
-
-        let wallGeo6 = new THREE.PlaneBufferGeometry(598, 300, 1);
-        let wall6 = new CreateWall(wallGeo6, wallMaterial, [-331.7,150,0], Math.PI/2);
-        scene.add(wall6);
-
-        let wallGeo7 = new THREE.PlaneBufferGeometry(663.4, 300, 1);
-        let wall7 = new CreateWall(wallGeo7, wallMaterial, [0,150,299], -Math.PI);
-        scene.add(wall7);
-
-        let wallGeo8 = new THREE.PlaneBufferGeometry(598, 300, 1);
-        let wall8 = new CreateWall(wallGeo8, wallMaterial, [331.7,150,0], -Math.PI/2);
-        scene.add(wall8);
-        
-        //wall group
-        let wallGrpElements = []
-        wallGrpElements.push([wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8]);
-
-        let wallGroup = new THREE.Group();
-        wallGroup.add(wallGrpElements);
-
-        function addFloor(shape, color, side, x, y, z, rx, ry, rz, s){
-            let  geometry = new THREE.ShapeBufferGeometry( shape );
-            
-            let mesh = new THREE.Mesh( geometry, new THREE.MeshStandardMaterial( { color: color, metalness:0.4, side : side} ) );
-            mesh.position.set( x, y, z);
-            mesh.rotation.set( rx, ry, rz );
-            mesh.scale.set( s, s, s );
-            mesh.receiveShadow = true;
-            mesh.castShadow = true;
-            group.add( mesh );
-        }
-
-        function addCornice(shape, depth, color, x, y, z, rx, ry, rz, s){
-            let extrudeSettings = { depth: depth, bevelEnabled: false};
-
-            let geometry = new THREE.ExtrudeBufferGeometry( shape, extrudeSettings );
-            let mesh = new THREE.Mesh( geometry, new THREE.MeshStandardMaterial( { color: color} ) );
-            mesh.position.set( x, y, z);
-            mesh.rotation.set( rx, ry, rz );
-            mesh.scale.set( s, s, s );
-            group.add( mesh );
-        }
-
-        let cornice = new THREE.Shape();
-        cornice.moveTo( 0, 0 );
-        cornice.lineTo( 0, 9 );
-        cornice.lineTo( 20, 9 );
-        cornice.lineTo( 20, 6 );
-        cornice.lineTo( 17, 6 );
-        cornice.lineTo( 17, 3 );
-        cornice.lineTo( 14, 3 );
-        cornice.lineTo( 14, 0 );
-        cornice.lineTo( 0, 0 );
-
-        //cornices
-        addCornice(cornice, 598, 0xffffff, -331.7, 290, -299, 0, 0, 0, 1);
-        addCornice(cornice, 663.4, 0xffffff, -331.7, 290, 299, 0, Math.PI/2, 0, 1);
-        addCornice(cornice, 598, 0xffffff, 331.7, 290, 299, 0, Math.PI, 0, 1);
-        addCornice(cornice, 214.5, 0xffffff, -117.2, 290, -299, 0, -Math.PI/2, 0, 1);
-        addCornice(cornice, 214.5, 0xffffff, 331.7, 290, -299, 0, -Math.PI/2, 0, 1);
-        addCornice(cornice, 304, 0xffffff, 117.2, 290, -299, 0, 0, 0, 1);
-        addCornice(cornice, 274.4, 0xffffff, 138, 290, 0, 0, -Math.PI/2, 0, 1);
-        addCornice(cornice, 304, 0xffffff, -137.2, 290, -299, 0, 0, 0, 1);
-        
-        let floorShape = new THREE.Shape();
-        floorShape.moveTo( 107.2, 0);
-        floorShape.lineTo( -107.2, 0 );
-        floorShape.lineTo( -107.2, -304 );
-        floorShape.lineTo( -341.6, -304 );
-        floorShape.lineTo( -341.6, 0 );
-        floorShape.lineTo( -556.3, 0 );
-        floorShape.lineTo( -556.3, -598 );
-        floorShape.lineTo( 107.2, -598 );
-        floorShape.lineTo( 107.2, 0 );
-
-        let ceilingShape = floorShape.clone();
-        //floor
-        addFloor(floorShape, 0xe5e5e5, THREE.FrontSide, 224.4, 0, -299, -Math.PI/2, 0, 0, 1);
-        //ceiling
-        addFloor(ceilingShape, 0xe5e5e5, THREE.BackSide, 224.4, 300, -299, -Math.PI/2, 0, 0, 1);
 
         //rect light inittialisation
         RectAreaLightUniformsLib.init();
@@ -173,9 +61,109 @@ import {CreateWall} from './CreateWall.js';
             
             Light.add( LightMesh );
 
-            // ceilingGroup.add(Light);
         }
         
+        let wallDimensions = [];
+        wallDimensions.splice(0, 0, [214.5, 300, 1]);
+        wallDimensions.splice(1, 0, [304, 300, 1]);
+        wallDimensions.splice(2, 0, [234.4, 300, 1]);
+        wallDimensions.splice(3, 0, [304, 300, 1]);
+        wallDimensions.splice(4, 0, [214.5, 300, 1]);
+        wallDimensions.splice(5, 0, [598, 300, 1]);
+        wallDimensions.splice(6, 0, [663.4, 300, 1]);
+        wallDimensions.splice(7, 0, [598, 300, 1]);
+
+        // walls
+        let wall1 = new CreateWall(wallDimensions[0], 0xffffff, [224.4,150,-299], 0);
+        scene.add(wall1);
+
+        let wall2 = new CreateWall(wallDimensions[1], 0xffffff, [117.2,150,-150], Math.PI/2);
+        scene.add(wall2);
+
+        let wall3 = new CreateWall(wallDimensions[2], 0xffffff, [0,150,2.5], 0);
+        scene.add(wall3);
+
+        let wall4 = new CreateWall(wallDimensions[3], 0xffffff, [-117.2,150,-150], -Math.PI/2);
+        scene.add(wall4);
+
+        let wall5 = new CreateWall(wallDimensions[4], 0xffffff, [-224.4,150,-299], 0);
+        scene.add(wall5);
+
+        let wall6 = new CreateWall(wallDimensions[5], 0xffffff, [-331.7,150,0], Math.PI/2);
+        scene.add(wall6);
+
+        let wall7 = new CreateWall(wallDimensions[6], 0xffffff, [0,150,299], -Math.PI);
+        scene.add(wall7);
+
+        let wall8 = new CreateWall(wallDimensions[7], 0xffffff, [331.7,150,0], -Math.PI/2);
+        scene.add(wall8);
+
+        //wall group
+        let wallGrpElements = []
+        wallGrpElements.push([wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8]);
+
+        let wallGroup = new THREE.Group();
+        wallGroup.add(wallGrpElements);
+
+        let cornice = new THREE.Shape();
+        cornice.moveTo( 0, 0 );
+        cornice.lineTo( 0, 9 );
+        cornice.lineTo( 20, 9 );
+        cornice.lineTo( 20, 6 );
+        cornice.lineTo( 17, 6 );
+        cornice.lineTo( 17, 3 );
+        cornice.lineTo( 14, 3 );
+        cornice.lineTo( 14, 0 );
+        cornice.lineTo( 0, 0 );
+
+        let corniceGroup = new THREE.Group();
+
+        //cornices
+        let corniceWall1 = new CreateCornice(cornice, wallDimensions[0][0], 0xffffff,[331.7, 290, -299], [0, -Math.PI/2, 0]);
+        corniceGroup.add(corniceWall1);
+
+        let corniceWall2 = new CreateCornice(cornice, wallDimensions[1][0], 0xffffff,[117.2, 290, -299], [0, 0, 0]);
+        corniceGroup.add(corniceWall2);
+
+        let corniceWall3 = new CreateCornice(cornice, 274.4, 0xffffff,[138, 290, 0], [0, -Math.PI/2, 0]);
+        corniceGroup.add(corniceWall3);
+
+        let corniceWall4 = new CreateCornice(cornice, wallDimensions[3][0], 0xffffff,[-137.2, 290, -299], [0, 0, 0]);
+        corniceGroup.add(corniceWall4);
+
+        let corniceWall5 = new CreateCornice(cornice, wallDimensions[4][0], 0xffffff,[-117.2, 290, -299], [0, -Math.PI/2, 0]);
+        corniceGroup.add(corniceWall5);
+
+        let corniceWall6 = new CreateCornice(cornice, wallDimensions[5][0], 0xffffff,[-331.7, 290, -299], [0, 0, 0]);
+        corniceGroup.add(corniceWall6);
+
+        let corniceWall7 = new CreateCornice(cornice, wallDimensions[6][0], 0xffffff,[-331.7, 290, 299], [0, Math.PI/2, 0]);
+        corniceGroup.add(corniceWall7);
+
+        let corniceWall8 = new CreateCornice(cornice, wallDimensions[7][0], 0xffffff,[331.7, 290, 299], [0, Math.PI, 0]);
+        corniceGroup.add(corniceWall8);
+
+        scene.add(corniceGroup);
+
+        let floorShape = new THREE.Shape();
+        floorShape.moveTo( 107.2, 0);
+        floorShape.lineTo( -107.2, 0 );
+        floorShape.lineTo( -107.2, -304 );
+        floorShape.lineTo( -341.6, -304 );
+        floorShape.lineTo( -341.6, 0 );
+        floorShape.lineTo( -556.3, 0 );
+        floorShape.lineTo( -556.3, -598 );
+        floorShape.lineTo( 107.2, -598 );
+        floorShape.lineTo( 107.2, 0 );
+
+        let ceilingShape = floorShape.clone();
+
+        let floor = new CreateFloor(floorShape,[0xe5e5e5, THREE.FrontSide],[224.4, 0, -299], [-Math.PI/2, 0, 0]);
+        scene.add(floor);
+
+        let ceiling = new CreateFloor(ceilingShape,[0xe5e5e5, THREE.BackSide],[224.4, 300, -299], [-Math.PI/2, 0, 0]);
+        scene.add(ceiling); 
+
         //light1
         RectangularLight(0xffffff,-224.4, 299, 147);
         //light2
@@ -184,7 +172,6 @@ import {CreateWall} from './CreateWall.js';
         RectangularLight(0xffffff,224.4, 299, -152);
         //light4
         RectangularLight(0xffffff,-224.4, 299, -152);
-
 
         // document.addEventListener( 'mousemove', onDocumentMouseMove, false );
         // document.addEventListener( 'mousedown', onDocumentMouseDown, false );
