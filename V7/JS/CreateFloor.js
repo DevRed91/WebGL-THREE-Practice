aa9c3458dc32556774f8f7397c4c2af16a7a9cd9
DevRed91 <51876334+DevRed91@users.noreply.github.com>
2020-06-10 14:16:15 +0530

    Update CreateFloor.js

--- V7/JS/CreateFloor.js
+++ V7/JS/CreateFloor.js
@@ -1,11 +1,12 @@
-class CreateCornice{
-    constructor (shape, [color, side],[positionX,positionY,positionZ], rotation){
+class CreateFloor{
+    constructor (shape, [color, side], [positionX,positionY,positionZ], [rotationX, rotationY, rotationZ]){
         let geometry = new THREE.ShapeBufferGeometry( shape );
         let material = new THREE.MeshStandardMaterial({ color: color, metalness:0.4, side : side})
 
         let mesh = new THREE.Mesh(geometry, material);
-        mesh.position.set(positionX,positionY,positionZ);
-        mesh.rotation.y = rotation;
+        mesh.position.set(positionX, positionY, positionZ);
+        mesh.rotation.set( rotationX, rotationY, rotationZ );
+        // mesh.scale.set( this.s, this.s, this.s);
         mesh.castShadow = true;
         mesh.recieveShadow = true;
 
@@ -14,7 +15,7 @@ class CreateCornice{
 
 }
 
-export{CreateCornice};
+export{CreateFloor};
 
 
 
